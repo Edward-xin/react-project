5caa180c02757a3e2fc07401b5bf3b10e2fd8de3
xiaolin <18859388361@163.com>
2020-01-14 20:59:40 +0800

    完成切换全屏功能

--- src/components/basic-layout/header-main/index.jsx
+++ src/components/basic-layout/header-main/index.jsx
@@ -1,15 +1,42 @@
 import React, { Component } from "react";
 import { Button, Icon } from "antd";
+import screenfull from "screenfull";
 
-import "./index.less"
+import "./index.less";
 
 export default class HeaderMain extends Component {
+  state = {
+    isScreenfull: false // 全屏标记 一开始不是全屏
+  };
+  componentDidMount() {
+    // 只需要绑定一次 所以在componentDidMount生命周期函数
+    // 检测全屏变化
+    screenfull.on("change", this.handleScreenFullChange);
+  }
+  componentWillUnmount() {
+    // 卸载组件要解绑事件 上面的screenfull.on('change'不是react事件 所以要解绑
+    screenfull.off("change", this.handleScreenFullChange);
+  }
+  // 解绑事件的回调函数
+  handleScreenFullChange = () => {
+    const { isScreenfull } = this.state;
+    this.setState({
+      isScreenfull: !isScreenfull
+    });
+  };
+
+  // 切换全屏的功能
+  screenFull = () => {
+    screenfull.toggle();
+  };
+
   render() {
+    const { isScreenfull } = this.state;
     return (
       <div className="header-main">
         <div className="header-main-top">
-          <Button size="small">
-            <Icon type="fullscreen" />
+          <Button size="small" onClick={this.screenFull}>
+            <Icon type={isScreenfull ? "fullscreen-exit" : "fullscreen"} />
           </Button>
           <Button size="small">English</Button>
           <span className="header-main-lang">hello,admin</span>

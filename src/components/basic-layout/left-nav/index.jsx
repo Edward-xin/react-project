e128fd2e1a266428ce227badd0914eb5fd2eeb6d
xiaolin <18859388361@163.com>
2020-01-14 20:05:26 +0800

    完成左侧导航功能

--- src/components/basic-layout/left-nav/index.jsx
+++ src/components/basic-layout/left-nav/index.jsx
@@ -1,51 +1,77 @@
-import React, { Component } from 'react'
-import {Menu,Icon} from 'antd'
-const { SubMenu,Item } = Menu;
+import React, { Component } from "react";
+import { Menu, Icon } from "antd";
+import { Link, withRouter } from "react-router-dom";
 
+import menus from "$conf/menus";
 
-export default class LeftNav extends Component {
-  render() {
+const { SubMenu, Item } = Menu;
+
+// withRouter高阶组件 提供路由组件的三大属性
+@withRouter
+class LeftNav extends Component {
+  // 遍历菜单项文件创建菜单
+  createMenus = menus => {
+    return menus.map(menu => {
+      if (menu.children) {
+        // 二级菜单
+        return (
+          <SubMenu
+            key={menu.path}
+            title={
+              <span>
+                <Icon type={menu.icon} />
+                <span>{menu.title} </span>
+              </span>
+            }
+          >
+            {menu.children.map(cMenu => this.createMenuItem(cMenu))};
+          </SubMenu>
+        );
+      } else {
+        // 一级菜单
+        return this.createMenuItem(menu);
+      }
+    });
+  };
+  // 给上面复用代码
+  createMenuItem = menu => {
     return (
-      <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-            <Item key="1">
-              <Icon type="home" />
-              <span>首页</span>
-            </Item>
-            <SubMenu
-              key="sub1"
-              title={
-                <span>
-                  <Icon type="appstore" />
-                  <span>商品</span>
-                </span>
-              }
-            >
-              <Item key="3">
-                <Icon type="bars" />
-                <span>分类管理</span>
-              </Item>
-              <Item key="4">
-              <Icon type="tool" />
-                <span>商品管理</span>
-              </Item>
-            </SubMenu>
-            <SubMenu
-              key="sub2"
-              title={
-                <span>
-                  <Icon type="team" />
-                  <span>Team</span>
-                </span>
-              }
-            >
-              <Item key="6">Team 1</Item>
-              <Item key="8">Team 2</Item>
-            </SubMenu>
-            <Item key="9">
-              <Icon type="file" />
-              <span>File</span>
-            </Item>
-          </Menu>
-    )
+      <Item key={menu.path}>
+        <Link to={menu.path}>
+          <Icon type={menu.icon} />
+          <span>{menu.title}</span>
+        </Link>
+      </Item>
+    );
+  };
+
+  // 找到需要展开的二级以上的菜单
+  findOpenKey = (pathname, menus) => {
+    const menu = menus.find(menu => {
+      if (menu.children) {
+        // 二级菜单 返回找到相同地址的菜单项
+        return menu.children.find(cMenu => cMenu.path === pathname);
+      }
+    });
+    if (menu) {
+      // 如果找得到 就返回这个菜单项的地址
+      return menu.path;
+    }
+  };
+
+  render() {
+    const { pathname } = this.props.location;
+    const openKey = this.findOpenKey(pathname, menus);
+    return (
+      <Menu
+        theme="dark" // 主题颜色
+        defaultSelectedKeys={[pathname]} // 默认选中的菜单
+        defaultOpenKeys={[openKey]} // 默认展开的菜单
+        mode="inline" // 展开子菜单方式
+      >
+        {this.createMenus(menus)}
+      </Menu>
+    );
   }
 }
+export default LeftNav;
